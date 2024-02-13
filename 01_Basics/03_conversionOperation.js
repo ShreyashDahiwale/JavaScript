6e891e214a61814591483d5f671f8c4da741445e
Shreyash Rajanand Dahiwale <61499836+ShreyashDahiwale@users.noreply.github.com>
2024-02-13 09:59:15 +0000

    Conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
-let score = "33abc";
+// let score = "33abc";
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof(valueInNumber));
-console.log(valueInNumber); // NaN --> Not a Number
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber); // NaN --> Not a Number
 
 /* 
     Number Conversion for 
@@ -18,3 +18,18 @@ console.log(valueInNumber); // NaN --> Not a Number
     "33" ==> 33
     "33abc" ==> NaN  but still its type is Number
 */
+
+
+let value = 3;
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Shereyash"
+let str = str1 + str2
+console.log(str);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + '2'); 
